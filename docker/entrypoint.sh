69bcc80eac5322b79873e1b3275bebc09f28fddd
Antonie Grauss <agraussgrimbergen@gmail.com>
2025-01-14 15:28:11 +0100

    Remove rosdep and copy from dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,10 @@
  
 # Source ROS 2
 source /opt/ros/${ROS_DISTRO}/setup.bash
+
+# Install all dependencies for the project
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
  
 # Build the project
 colcon build
