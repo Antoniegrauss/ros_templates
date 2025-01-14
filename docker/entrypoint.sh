2a87a1e8c1bcf3999504712951d8a5ada496acc6
Antonie Grauss <agraussgrimbergen@gmail.com>
2025-01-14 15:42:31 +0100

    Try fixing gitlab ci build

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 
 # Install all dependencies for the project
 rosdep update
-rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
+rosdep install --from-paths src/ --ignore-src --rosdistro ${ROS_DISTRO} -y
  
 # Build the project
 colcon build
